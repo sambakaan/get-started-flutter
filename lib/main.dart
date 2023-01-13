53539be8d18e89251e71fed557346c8cf180acb9
Samba Bery KANE <sambabery.kane@gmail.com>
2023-01-13 01:34:01 +0000

    Ajout du design de base de l'application

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Bonjour simen simplon'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Simplon SIMEN'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text('Bienvenu dans la formation flutter'),
+              Text('12'),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          tooltip: 'Incrementer',
+          child: Icon(Icons.add),
+        ),
+      ),
     );
   }
 }
