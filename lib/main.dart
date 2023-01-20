de57d521f4a76325ce48e85dffb930fb1b683c6e
Samba Bery KANE <sambabery.kane@gmail.com>
2023-01-20 13:52:17 +0000

    Decoupage des pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:test_flutter_app/widgets/message_info.dart';
+import './pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
-  State<StatefulWidget> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int nbreWird = 0;
-  int wirdIndex = 0;
-  List<String> wirds = const [
-    'subhanallah',
-    'walhamdulillah',
-    'wala ilha illalah',
-    'wallahu akbar',
-    'la illaha illalah',
-  ];
-
-  void addWird() {
-    setState(() {
-      nbreWird++;
-      if (nbreWird > 33 && nbreWird < 66) {
-        wirdIndex = 1;
-      } else if (nbreWird > 66 && nbreWird < 99) {
-        wirdIndex = 2;
-      } else if (nbreWird > 99 && nbreWird < 132) {
-        wirdIndex = 3;
-      } else if (nbreWird > 132) {
-        wirdIndex = 1;
-      }
-    });
-  }
-
-  void resetWird() {
-    setState(() {
-      wirdIndex = 0;
-      nbreWird = 0;
-    });
-  }
-
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Simplon SIMEN'),
-          actions: [
-            IconButton(onPressed: resetWird, icon: const Icon(Icons.restore))
-          ],
-          backgroundColor: Colors.green,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                "assets/images/zikrrr-logo.png",
-                fit: BoxFit.cover,
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Column(
-                children: [
-                  MessageInfo(message: wirds.elementAt(wirdIndex)),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  MessageInfo(message: nbreWird.toString()),
-                ],
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: addWird,
-          tooltip: 'Tasbih',
-          backgroundColor: Colors.green,
-          child: const Icon(Icons.add),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      ),
+      home: HomePage(),
     );
   }
 }
